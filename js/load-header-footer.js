2a32b87fe642ba64f4691ce585a13781b2ec4ea3
cuonghuynh8826 <cuonghuynh8826@gmail.com>
2026-03-28 03:22:08 +0000

    them: header footer dung chung

---
+++ js/load-header-footer.js
@@ -0,0 +1,9 @@
+//lay header
+fetch('../layouts/header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header-container').innerHTML = data);
+
+// lay footer
+fetch('../layouts/footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer-container').innerHTML = data);
